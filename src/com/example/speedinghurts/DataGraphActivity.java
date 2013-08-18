42d7e9f521ac446d23be7ad29ef696972a33555b
Paul Khermouch <pkhermouch@gmail.com>
2013-08-17 23:15:16 -0400

    * Added a nicer graph, although the SVG one's text won't display * Updated TODO to reflect the change * Added libs/ folder, which currently contains the SVG library (which isn't in use at the moment)

--- src/com/example/speedinghurts/DataGraphActivity.java
+++ src/com/example/speedinghurts/DataGraphActivity.java
@@ -11,6 +11,10 @@ import android.annotation.SuppressLint;
 import android.os.Build;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
+/*
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+*/
 
 public class DataGraphActivity extends Activity {
 
@@ -21,9 +25,20 @@ public class DataGraphActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         ImageView image = new ImageView(this);
-        image.setImageResource(R.raw.graph);
+        
+        // Import the SVG graph
+        /*
+        SVG svg = SVGParser.getSVGFromResource(getResources(),
+                                               R.raw.graph_scalable);
+        // Get a drawable from the parsed SVG and set it as the drawable for the ImageView
+        image.setImageDrawable(svg.createPictureDrawable());
+        */
+        
+        image.setImageResource(R.raw.graph_static);
+        
         // Stretch to fill screen
         // image.setScaleType(ScaleType.FIT_XY);
+        
         setContentView(image);
         
     }
